7aac93d025d4021ec26fe0c81c98588b19af1535
ndourmamadou2005-collab <ndourmamadou2005@gmail.com>
2026-02-10 11:30:52 +0000

    Mise à jour du projet

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -41,7 +41,7 @@ function Sidebar() {
         </ul>
 
         <div className="user">
-          <p>Amadou Aw</p>
+          <p>MAMADOU NDOUR</p>
           <span>● en ligne</span>
         </div>
       </div>
